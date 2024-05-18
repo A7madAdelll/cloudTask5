e1d113f5fe2f1481f27ca0c76960963de1d978e8
A7madAdelll <official3adool@gmail.com>
2024-05-18 09:25:06 +0300

    saba7 el ward 6

--- run_ls.bat
+++ run_ls.bat
@@ -1,5 +1,11 @@
+echo "# elbonuscloud" >> README.md
 git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
 git remote add origin https://github.com/A7madAdelll/elbonuscloud.git
+git push -u origin main
+
 git branch -M main
 git push -u origin main  git config --global user.email "ahmadaa145632@gmail.com"
   git config --global user.name "dola"
