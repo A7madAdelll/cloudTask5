25403337aa2500de77ed0da53cdd6eb6669695d3
A7madAdelll <official3adool@gmail.com>
2024-05-18 09:22:37 +0300

    saba7 el ward 5

--- run_ls.bat
+++ run_ls.bat
@@ -1,5 +1,7 @@
-git clone "https://github.com/A7madAdelll/elbonuscloud.git"
-  git config --global user.email "ahmadaa145632@gmail.com"
+git init
+git remote add origin https://github.com/A7madAdelll/elbonuscloud.git
+git branch -M main
+git push -u origin main  git config --global user.email "ahmadaa145632@gmail.com"
   git config --global user.name "dola"
 dir
 echo Hello, world! > myfile2.txt
