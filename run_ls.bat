e523b43acac4112a4b3b014201297531b1e0595d
A7madAdelll <official3adool@gmail.com>
2024-05-18 09:04:36 +0300

    saba7 el ward 2

--- run_ls.bat
+++ run_ls.bat
@@ -1,2 +1,6 @@
-echo Hello, world! > myfile.txt
+git clone "https://github.com/A7madAdelll/elbonuscloud.git"
 dir
+echo Hello, world! > myfile.txt
+git add .
+git commit -m "add a file from onother repo"
+git push
